e116eab084df30834e82cb2072ae9b6e7e6d7bd6
Tatiana Thorpe <tthorpe@utilitywarehouse.co.uk>
2018-03-27 07:50:19 +0100

    add faker function

--- main.go
+++ main.go
@@ -7,10 +7,6 @@ import (
 )
 
 func main() {
-	fake, err := faker.New("en")
-	if err != nil {
-		panic(err)
-	}
 
 	var number int
 	fmt.Print("Please enter the number of records you require: ")
@@ -19,16 +15,26 @@ func main() {
 
 	for i < number {
 		i++
-		title := strings.ToUpper(fake.NamePrefix())
-		name := strings.ToUpper(fake.Name())
-
-		println("START OF RECORD", i)
-		println("NAME:", title, name)
-		println("EMAIL:", fake.Email())
-		println("ADDRESS:", fake.StreetAddress(), fake.City(), fake.PostCode(), fake.State(), fake.Country())
-		println("PHONE NUMBER:", fake.PhoneNumber())
-		println("MOBILE:", fake.CellPhoneNumber(), "\n")
+		println("RECORD", i)
+		dummy()
 	}
 
 	fmt.Println("RECORDS GENERATED:", number)
+}
+
+func dummy() {
+	fake, err := faker.New("en")
+	if err != nil {
+		panic(err)
+	}
+
+	title := strings.ToUpper(fake.NamePrefix())
+	name := strings.ToUpper(fake.Name())
+
+	println("NAME:", title, name)
+	println("EMAIL:", fake.Email())
+	println("ADDRESS:", fake.StreetAddress(), fake.City(), fake.PostCode(), fake.State(), fake.Country())
+	println("PHONE NUMBER:", fake.PhoneNumber())
+	println("MOBILE:", fake.CellPhoneNumber())
+	println("-----------------------","\n")
 }
