195fcea6c44332475610066f49211c4c44e14257
Tatiana Thorpe <tthorpe@utilitywarehouse.co.uk>
2018-03-29 12:35:11 +0100

    more tweaks

--- main.go
+++ main.go
@@ -3,19 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/TatianaThorpe/learnGoProject/dataGenerator"
-	//"go/types"
 )
 
 func main() {
 
 	var number int
 	fmt.Print("Please enter the number of records you require: ")
-	fmt.Scanf("%d", &number)
+	fmt.Scan(&number)
 	i := 0
 
 	for i < number {
 		i++
-		println("\n---------- RECORD", i,"----------")
+		println("\n---------- RECORD", i, "----------")
 		dataGenerator.Dummy()
 	}
 
