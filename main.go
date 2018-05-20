bb963fe7ce59cf1041f38879c434d39bf8d072ef
Tatiana Thorpe <tthorpe@utilitywarehouse.co.uk>
2018-05-20 19:03:16 +0100

    validate input

--- main.go
+++ main.go
@@ -2,21 +2,58 @@ package main
 
 import (
 	"fmt"
-	"github.com/TatianaThorpe/learnGoProject/dataGenerator"
+	"github.com/manveru/faker"
+	"strings"
+	"os"
+)
+
+var (
+	min = 1
+	max = 10000
 )
 
 func main() {
-
-	var number int
-	fmt.Print("Please enter the number of records you require: ")
-	fmt.Scan(&number)
+	number := userinput()
 	i := 0
+	countryMatch := 0
 
 	for i < number {
 		i++
 		println("\n---------- RECORD", i, "----------")
-		dataGenerator.Dummy()
+		fake, err := faker.New("en")
+		if err != nil {
+			panic(err)
+		}
+
+		title := strings.ToUpper(fake.NamePrefix())
+		name := strings.ToUpper(fake.Name())
+		country := fake.Country()
+
+		fmt.Println("NAME:", title, name)
+		fmt.Println("EMAIL:", fake.Email())
+		fmt.Println("ADDRESS:", fake.StreetAddress(), fake.City(), fake.PostCode(), fake.State(), country)
+		fmt.Println("PHONE NUMBER:", fake.PhoneNumber())
+		fmt.Println("MOBILE:", fake.CellPhoneNumber())
+
+		if country == "United Kingdom" || country == "United States of America" {
+			countryMatch++
+		}
+
 	}
 
 	fmt.Println("\nRECORDS GENERATED:", number)
+	fmt.Println("Number of UK or USA records found:", countryMatch)
+}
+
+func userinput() int {
+	var number int
+
+	fmt.Printf("Please enter a number between %d and %d: ", min, max)
+	fmt.Scan(&number)
+
+	if number < min || number > max {
+		fmt.Printf("Please enter a number between %d and %d\n", min, max)
+		os.Exit(1)
+	}
+	return number
 }
